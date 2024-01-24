78102c5eee2f0e49d4b9d64e67591b7681f76791
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2024-01-24 19:25:11 +0700

    menampilkan list movie secara dinamis

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,71 +1,36 @@
+import { useEffect, useState } from "react";
+
 function MovieList() {
-    return (
-        <div className="row">
-            <div className="col-sm-4 mb-2">
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title">Special title treatment</h5>
-                        <p className="card-text">
-                            With supporting text below as a natural lead-in to additional content.
-                        </p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    setMovies([
+      { id: 1, title: "Spongebob Squarepants", runTime: 145 },
+      { id: 2, title: "Ogry", runTime: 145 },
+      { id: 3, title: "Chalk Zone", runTime: 145 },
+    ]);
+  }, []);
+
+  return (
+    <div className="row">
+      {movies.map((movie, index) => (
+        <div key={index} className="col-sm-4 mb-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">{movie.title}</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
             </div>
-            <div className="col-sm-4 mb-2">
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title">Special title treatment</h5>
-                        <p className="card-text">With supporting text below as a natural lead-in to
-                            additional content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div className="col-sm-4 mb-2">
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title">Special title treatment</h5>
-                        <p className="card-text">
-                            With supporting text below as a natural lead-in to additional content.
-                        </p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div className="col-sm-4 mb-2">
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title">Special title treatment</h5>
-                        <p className="card-text">With supporting text below as a natural lead-in to
-                            additional content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div className="col-sm-4 mb-2">
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title">Special title treatment</h5>
-                        <p className="card-text">
-                            With supporting text below as a natural lead-in to additional content.
-                        </p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <div className="col-sm-4 mb-2">
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title">Special title treatment</h5>
-                        <p className="card-text">With supporting text below as a natural lead-in to
-                            additional content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default MovieList;
