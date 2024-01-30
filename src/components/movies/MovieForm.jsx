827a561b2729788d291eaddec9431540dcedc296
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2024-01-30 11:56:45 +0700

    menggunakan react hook form

--- src/components/movies/MovieForm.jsx
+++ src/components/movies/MovieForm.jsx
@@ -1,32 +1,63 @@
-function MovieForm(props) {
+import { useForm } from 'react-hook-form';
+
+function MovieForm() {
+   const { register, handleSubmit, setValue } = useForm();
+
+   const onSubmit = (data) => {
+      console.log(data);
+   };
+
    return (
       <>
          <h2>Movie Form</h2>
          <hr />
-         <form action="">
+         <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-3">
                <label htmlFor="title" className={'form-label'}>
                   Title
                </label>
-               <input type="text" className={'form-control'} id={'title'} name={'title'} />
+               <input
+                  type="text"
+                  className={'form-control'}
+                  id={'title'}
+                  name={'title'}
+                  {...register('title', { required: true })}
+               />
             </div>
             <div className="mb-3">
                <label htmlFor="release_date" className={'form-label'}>
                   Release Data
                </label>
-               <input type="date" className={'form-control'} id={'release_date'} name={'release_date'} />
+               <input
+                  type="date"
+                  className={'form-control'}
+                  id={'release_date'}
+                  name={'release_date'}
+                  {...register('release_date', { required: true })}
+               />
             </div>
             <div className="mb-3">
-               <label htmlFor="release_date" className={'form-label'}>
+               <label htmlFor="runtime" className={'form-label'}>
                   Runtime
                </label>
-               <input type="number" className={'form-control'} id={'release_date'} name={'release_date'} />
+               <input
+                  type="number"
+                  className={'form-control'}
+                  id={'runtime'}
+                  name={'runtime'}
+                  {...register('runtime', { required: true })}
+               />
             </div>
             <div className="mb-3">
                <label htmlFor="mppaa_rating" className={'form-label'}>
                   MPPAA Rating
                </label>
-               <select name="mppaa_rating" id="mppaa_rating" className={'form-control'}>
+               <select
+                  name="mppaa_rating"
+                  id="mppaa_rating"
+                  className={'form-control'}
+                  {...register('mppaa_rating', { required: true })}
+               >
                   <option value="G" className={'form-control'}>
                      G
                   </option>
@@ -48,16 +79,29 @@ function MovieForm(props) {
                <label htmlFor="rating" className={'form-label'}>
                   Rating
                </label>
-               <input type="number" className={'form-control'} id={'rating'} name={'rating'} />
+               <input
+                  type="number"
+                  className={'form-control'}
+                  id={'rating'}
+                  name={'rating'}
+                  {...register('rating', { required: true })}
+               />
             </div>
             <div className="mb-3">
                <label htmlFor="description" className={'form-label'}>
                   Description
                </label>
-               <textarea className={'form-control'} id={'description'} name={'description'} />
+               <textarea
+                  className={'form-control'}
+                  id={'description'}
+                  name={'description'}
+                  {...register('description', { required: true })}
+               />
             </div>
             <hr />
-            <button className={'btn btn-primary mb-4'}>Save</button>
+            <button type={'submit'} className={'btn btn-primary mb-4'}>
+               Save
+            </button>
          </form>
       </>
    );
